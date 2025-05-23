2ce8efc855985480464290bbaddadf0ca31f6a1f
madhi13 <velusamymadhimalar@gmail.com>
2025-05-23 10:44:59 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (67.68%) - LeetSync

---
+++ 1944-truncate-sentence/truncate-sentence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string truncateSentence(string s, int k) {
+        string ans = "";
+        for(int i = 0; i < s.length();i++){
+            if(s[i] == ' ') {
+                k--;
+                if(k == 0) break;
+                ans += " ";
+            } else {
+                    ans += s[i];                
+            }
+        }
+        return ans;
+     }
+};
