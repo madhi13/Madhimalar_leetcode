028291e3828821e722a556dae21e1d967eaafd13
madhi13 <velusamymadhimalar@gmail.com>
2025-05-24 15:05:15 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (20.58%) - LeetSync

---
+++ 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool areAlmostEqual(string s1, string s2) {
+        if (s1 == s2) return true;
+        vector<int> diff;
+        for (int i = 0; i < s1.size(); ++i) {
+            if (s1[i] != s2[i]) {
+                diff.push_back(i);
+            }
+        }
+        if (diff.size() != 2) return false;
+
+        int i = diff[0], j = diff[1];
+        return s1[i] == s2[j] && s1[j] == s2[i];
+    }
+};
+
+
